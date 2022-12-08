10f01664c6e6452c85f9b6bdb8234aabd711dc29
FKRIK <fernandokrik@hotmail.com>
2022-12-08 19:51:08 -0300

    adiciona entity framework

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IMC_aplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMC_aplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Imc> Imcs { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IMC_aplication.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,20 @@ namespace IMC_aplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(
+                options =>
+                {
+                    options.AddPolicy("CorsPolicy", builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                }
+            );
+
+            services.AddDbContext<DataContext>
+            (
+                options => options.UseSqlite("DataSource=fernando.db;Cache=shared")
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
